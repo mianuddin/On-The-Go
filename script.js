ddcdcb44d44291053ac3f976b70b03bc5f75dc45
Mian Uddin <mianuddin@gmail.com>
2015-05-03 03:17:14 +0000

    Optimized map centering; moved coordinate-related functions to 'coords' module.

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2-lat1);  // deg2rad below
-    var dLon = deg2rad(lon2-lon1); 
-    var a = 
-        Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-        Math.sin(dLon/2) * Math.sin(dLon/2); 
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-    var d = R * c; // Distance in km
-    return d;
-}
-function deg2rad(deg) {
-    return deg * (Math.PI/180)
-}
-// function findClosestBathroom(json) {
-// }
-function initialize() {
+// var onTheGo = onTheGo || {};
+navigator.geolocation.getCurrentPosition(function (position) {
+    google.maps.event.addDomListener(window, 'load', initializeMap(position));
+    console.log("geolocation enabled");
+},
+function (error) { 
+  if (error.code == error.PERMISSION_DENIED)
+      console.log("geolocation denied");
+});
+
+function initializeMap(position) {
     var mapOptions = {
-        center: { lat: 37.765, lng: -122.242}, // coords of Alameda
+        center: { lat: position.coords.latitude, lng: position.coords.longitude},
         zoom: 18
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            var initialLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            map.setCenter(initialLocation);
-        });
-    }
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+
+var coords = (function () {
+    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = deg2rad(lat2-lat1);  // deg2rad below
+        var dLon = deg2rad(lon2-lon1); 
+        var a = 
+            Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+            Math.sin(dLon/2) * Math.sin(dLon/2); 
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+        var d = R * c; // Distance in km
+        return d;
+    }
+    function deg2rad(deg) {
+        return deg * (Math.PI/180)
+    }
+});
